ba7984124be8b4db2f782b46cb568df0b904f150
JarbasJunior <jarbasn3249@gmail.com>
2021-06-19 18:52:21 -0300

    conex

--- db/connect.php
+++ db/connect.php
@@ -5,10 +5,10 @@
 //$password   = "";
 //$dbname     = "notas";
 
-define('HOST','localhost');
-	define('DB','notas');
-	define('USER','root');
-	define('PASS','');
+define('HOST','	sql307.epizy.com');
+define('DB','epiz_28918882_XXX');
+define('USER','epiz_28918882');
+define('PASS','6xwKZrxRtyfT15');
 
 try {
   $pdo = new PDO('mysql:host='.HOST.';dbname='.DB,USER,PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
