cca1c9d1a325235b00b4669be7b2abec7d4f08c2
JarbasJunior <jarbasn3249@gmail.com>
2021-06-21 15:13:30 -0300

    3,

--- crud/update.php
+++ crud/update.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update</title>
+</head>
+
 <?php 
 
 define('HOST','	sql307.epizy.com');
@@ -14,16 +24,6 @@ define('PASS','6xwKZrxRtyfT15');
 	}
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
 <body>
 
